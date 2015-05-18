1a0d21cacbc238cb61cc7ac4f1a70d071cce86a5
ppedraza <ppedraza@domasolutions.com>
2015-05-18 14:59:55 -0300

    testtt

--- protected/tests/WebTestCase.php
+++ protected/tests/WebTestCase.php
@@ -2,7 +2,7 @@
 
 /**
  * Change the following URL based on your server configuration
- * Makess sure the URL ends with a slash so that we can use relative URLs in test cases
+ * Makesss1 sure the URL ends with a slash so that we can use relative URLs in test cases
  */
 define('TEST_BASE_URL','http://localhost/testdrive/index-test.php/');
 
