7e57aaea464e2eadba8b294a2dd9e129e440bec9
arnaldomontiel <arnaldomontiel@gmail.com>
2015-05-28 15:14:19 -0300

    agregado boton volver

--- protected/views/customer/view.php
+++ protected/views/customer/view.php
@@ -3,7 +3,8 @@
     <div class="col-sm-6">
   <h1 class="pageTitle">Clientes</h1>
   </div>
-    <div class="col-sm-6 align-right">
+  <div class="col-sm-6 align-right">
+           <button type="button" class="btn btn-default marginLeft pull-right" onclick="window.location = '<?php echo CustomerController::createUrl('admin')?>'"><i class="fa fa-arrow-left fa-fw"></i> Volver</button>
   </div>
   </div>
   <div class="row">
@@ -11,23 +12,70 @@
     <?php 
     $this->widget('zii.widgets.CDetailView', array(
     		'data'=>$model,
+    		'htmlOptions' => array('class'=>'table table-striped table-bordered'
+    		),
     		'attributes'=>array(
     				'person.name',
     				'person.last_name',
     				'contact.description',
-    				'contact.telephone_1',
-    				'contact.telephone_2',
-    				'contact.telephone_3',
-    				'contact.email',
-    				'contact.email_2',
-    				'contact.email_3',
+    				array(
+    						'label'=>'Telefono 1 ('.$model->contact->tel1_description.')',
+    						'type'=>'raw',
+    						'value'=>$model->contact->telephone_1,
+    				),
+    				array(
+    						'label'=>'Telefono 2 ('.$model->contact->tel2_description.')',
+    						'type'=>'raw',
+    						'value'=>$model->contact->telephone_2,
+    				),
+    				array(
+    						'label'=>'Telefono 3 ('.$model->contact->tel3_description.')',
+    						'type'=>'raw',
+    						'value'=>$model->contact->telephone_3,
+    				),
+    				array(
+    						'label'=>'Correo 1 ('.$model->contact->email_description.')',
+    						'type'=>'raw',
+    						'value'=>(isset($model->contact->email)?$model->contact->email:""),
+    				),
+    				array(
+    						'label'=>'Correo 2 ('.$model->contact->email_2_description.')',
+    						'type'=>'raw',
+    						'value'=>(isset($model->contact->email_2)?$model->contact->email_2:""),
+    				),
+    				array(
+    						'label'=>'Correo 3 ('.$model->contact->email_3_description.')',
+    						'type'=>'raw',
+    						'value'=>(isset($model->contact->email_3)?$model->contact->email_3:""),
+    				),
+    				array(
+    						'label'=>'Notas',
+    						'type'=>'raw',
+    						'value'=>(isset($model->contact->comment)?$model->contact->comment:""),
+    				),
     		),
-    ));
-    
+    ));    
 	?>
-    
     </div>
     <!-- /.col-sm-12 --> 
   </div>
-  <!-- /.row --> 
+  <!-- /.row -->
+  <div class="row">
+  <div class="col-sm-6">
+  	<h1 class="pageTitle">Direcciones</h1>
+  </div>
+  </div>  
+      <?php
+     foreach($model->projects as $project)
+     {?>
+       <div class="row">
+  		<div class="col-sm-6">
+     <?php
+     	echo $project->description.": ".$project->address;
+     	?>
+     	  </div>
+	  </div>
+     	<?php 
+     }
+    ?>
 </div>
